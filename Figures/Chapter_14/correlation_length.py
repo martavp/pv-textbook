91f0effe278111d6d04753424bd41695c25563d9
martavp <martavictoriaperez@gmail.com>
2023-03-15 19:46:33 +0100

    Add problem 14.6 and improve others

--- Figures/Chapter_14/correlation_length.py
+++ Figures/Chapter_14/correlation_length.py
@@ -10,35 +10,36 @@ from netCDF4 import Dataset
 import geopy.distance
 plt.style.use(['seaborn-ticks','../pv-textbook.mplstyle'])
 
-colors=['orange',
-        'dodgerblue',
-        'black',]
-
 nc = Dataset('data/cutout/europe-2013-era5.nc')
 
 data=pd.DataFrame(data=None, 
                   index=None, 
-                  columns=['lon', 'lat', 'distance','ro_wind', 'ro_radiation'], 
+                  columns=['lon', 
+                           'lat', 
+                           'distance',
+                           'rho_wind', 
+                           'rho_radiation'], 
                   dtype=float)
 
 
-latitude = 52.52 #Berlin
-longitude = 13.40
+latitude = 48.86 #Paris
+longitude = 2.34
 index_lat = np.argmin(np.abs(nc.variables['lat'][:].data-latitude))
 index_lon = np.argmin(np.abs(nc.variables['lon'][:].data-longitude))
+
 
 lon_ref=nc.variables['lon'][index_lon].data
 lat_ref=nc.variables['lat'][index_lat].data
 wind_ref=nc.variables['wnd100m'][:,index_lat,index_lon].data
 radiation_ref = nc.variables['influx_direct'][:,index_lat,index_lon].data+nc.variables['influx_diffuse'][:,index_lat,index_lon].data
 
+i_min=max(0,index_lon-75)
+i_max=min(len(nc.variables['lon'][:].data),index_lon+75)
+j_min=max(0,index_lat-75)
+j_max=min(len(nc.variables['lat'][:].data),index_lat+75)
 k=0
-i_min=max(0,index_lon-40)
-i_max=min(len(nc.variables['lon'][:].data),index_lon+40)
-j_min=max(0,index_lat-40)
-j_max=min(len(nc.variables['lat'][:].data),index_lat+40)
-for i in np.arange(i_min,i_max): #np.arange(0,len(nc.variables['lon'][:].data)):
-    for j in np.arange(j_min,j_max): #np.arange(0,len(nc.variables['lat'][:].data)):
+for i in np.arange(i_min,i_max, 10):        
+    for j in np.arange(j_min,j_max, 10):    
         k=k+1
         lon=nc.variables['lon'][i].data
         data.loc[k, 'lon'] = lon
@@ -51,13 +52,13 @@ for i in np.arange(i_min,i_max): #np.arange(0,len(nc.variables['lon'][:].data)):
         
         wind = nc.variables['wnd100m'][:,j,i]
         radiation = nc.variables['influx_direct'][:,j,i].data+nc.variables['influx_diffuse'][:,j,i].data
-        data.loc[k,'ro_wind'] = np.corrcoef(wind,wind_ref)[0,1]
-        data.loc[k,'ro_radiation'] = np.corrcoef(radiation,radiation_ref)[0,1]
+        data.loc[k,'rho_wind'] = np.corrcoef(wind,wind_ref)[0,1]
+        data.loc[k,'rho_radiation'] = np.corrcoef(radiation,radiation_ref)[0,1]
         
 #%%
 
 data.to_csv('data_correlation.csv', sep=',')
-data2=pd.read_csv('data_correlation.csv', sep=',', index_col=0)
+data_s=pd.read_csv('data_correlation.csv', sep=',', index_col=0)
 plt.figure(figsize=(12, 6))
 gs = gridspec.GridSpec(1, 2)
 gs.update(wspace=0.1, hspace=0.1)
@@ -65,7 +66,7 @@ ax0 = plt.subplot(gs[0,0])
 ax0.set_title('Wind velocity')
 ax0.set_ylabel(r'Correlation coefficient $\rho_{g_t^{R_i},g_t^{R_j}}$')
 ax0.set_xlabel('distance (km)')
-ax0.plot(data2['distance'],data2['ro_wind'],
+ax0.plot(data_s['distance'],data_s['rho_wind'],
          marker='.', 
          markersize=2,
          linewidth=0,
@@ -80,7 +81,7 @@ ax1.set_yticklabels([])
 ax1.set_ylim([-0.2,1])
 ax1.set_xlim([0,3000])
 ax1.set_xlabel('distance (km)')
-ax1.plot(data2['distance'],data2['ro_radiation'],
+ax1.plot(data_s['distance'],data_s['rho_radiation'],
          marker='.', 
          markersize=1,
          linewidth=0,
@@ -93,6 +94,6 @@ ax0.plot(dis,cor_theo_wind, 'gray', linewidth=3)
 
 CL_radiation=600*4
 cor_theo_radiation=[np.exp(-(1/CL_radiation)*d) for d in dis]
-#ax1.plot(dis,cor_theo_radiation, 'gray', linewidth=3)
+
 plt.savefig('figures/correlation_length.png', 
             dpi=300, bbox_inches='tight')
